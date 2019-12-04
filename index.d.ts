b1ec10d904af8ecb355d661023e0c709877def8a
pankaj-pp <pnkajpande@gmail.com>
2019-12-04 17:34:04 +0530

    refactor(types): update types

--- index.d.ts
+++ index.d.ts
@@ -17,8 +17,8 @@ export interface CarouselProps {
     showArrows?: boolean;
     showStatus?: boolean;
     showIndicators?: boolean;
-    leftArrow: React.JSX.Element,
-    rightArrow: React.JSX.Element,
+    leftArrow?: React.JSX.Element,
+    rightArrow?: React.JSX.Element,
     showThumbs?: boolean;
     infiniteLoop?: boolean;
     selectedItem?: number;
